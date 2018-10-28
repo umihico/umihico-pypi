efd03346bd0a99f8ae9a709684f2b23d3cc7b4bc
umihico <umihico_dummy@users.noreply.github.com>
2018-10-28 17:10:55 +0900

    version14

--- umihico/hash_/__init__.py
+++ umihico/hash_/__init__.py
@@ -3,6 +3,10 @@ import hashlib as _hashlib
 
 def hash_text(text):
     sha256 = _hashlib.sha256()
-    sha256.update(text)
+    sha256.update(text.encode())
     hashed_text = sha256.hexdigest()
     return hashed_text
+
+
+if __name__ == '__main__':
+    print(hash_text('text'))
