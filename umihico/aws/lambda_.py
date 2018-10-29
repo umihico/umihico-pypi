8e29eb0200a92d6603410eba46883920f950327c
umihico <umihico_dummy@users.noreply.github.com>
2018-10-29 18:42:32 +0900

    version19

--- umihico/aws/lambda_.py
+++ umihico/aws/lambda_.py
@@ -2,7 +2,9 @@
 
 import requests as _requests
 import json as _json
-import functools
+import functools as _functools
+import pprint as _pprint
+import traceback as _traceback
 
 
 def trigger_via_apigateway(url, api_key=None, payload=None):
@@ -15,26 +17,31 @@ def trigger_via_apigateway(url, api_key=None, payload=None):
     return _requests.request(method, url, **kw)
 
 
-def lambda_apigateway_wrapper(lambda_handler):
-    @functools.wraps(lambda_handler)
+def apigateway_wrapper(lambda_handler):
+    @_functools.wraps(lambda_handler)
     def lambda_handler_wrapper(event, context):
         try:
             body = lambda_handler(event, context)
             statusCode = 200
         except Exception as e:
             statusCode = 500
-            body = str(e)
             _traceback.print_exc()
             body = _traceback.format_exc()
-        finally:
-            return {"statusCode": statusCode, "body": str(body)}
-    return hoge_wrapper
+        # finally:
+        return {"statusCode": statusCode, "body": str(body)}
+    return lambda_handler_wrapper
 
 
+@apigateway_wrapper
 def lambda_handler(event, context):
+    raise
+    # return 'test'
 
 
 if __name__ == '__main__':
+    """
+    returns "test!" if lambda_handler is like this.
+
     def test_trigger_via_apigateway():
         payload = {
             "api_key": "test!",
@@ -43,16 +50,12 @@ if __name__ == '__main__':
         url = "https://k0ch7iuaj4.execute-api.us-west-2.amazonaws.com/default/asyc_superfast_receiver"
         api_key = "JJnYqNMTBU1LdIEjaSYL43G4OGfysL9B1In5oPoy"
         return trigger_via_apigateway(url, api_key, payload)
-
     import ast
-
     def lambda_handler(event, context):
-        """returns "test!"  if lambda_handler is like this."""
         payload = ast.literal_eval(event['body'])
         return {
             "statusCode": 200,
             "body": payload['api_key']
         }
-
-    response = test_trigger_via_apigateway()
-    print(response.text)
+    """
+    print(lambda_handler(None, None))
