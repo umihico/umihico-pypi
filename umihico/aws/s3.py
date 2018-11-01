6641c2129ac550c141a60971fd3b8d2ce77cbabd
umihico <umihico_dummy@users.noreply.github.com>
2018-11-01 21:32:17 +0900

    version34

--- umihico/aws/s3.py
+++ umihico/aws/s3.py
@@ -1,4 +1,5 @@
 import boto3 as _boto3
+import base64 as _base64
 
 
 def gen_s3(key, secret_key, region_name):
@@ -14,21 +15,22 @@ def gen_s3(key, secret_key, region_name):
 def download_text(bucketname, path, s3=None):
     s3 = s3 or _boto3.resource('s3')
     obj = s3.Object(bucketname, path)
-    text = obj.get()['Body'].read().decode('utf-8')
+    text = _base64.b64decode(obj.get()['Body'].read()).decode()
     return text
 
 
-def upload_text(bucketname, path, text, public_read=False, ContentType='text/plain; charset=utf-8', s3=None):
+def upload_text(bucketname, path, text, public_read=False, ContentType='text/plain;charset=utf-8', s3=None):
     s3 = s3 or _boto3.resource('s3')
     acl = {True: "public-read",
            False: "private"}[public_read]
-    response = s3.Bucket(bucketname).put_object(ACL=acl, Body=text,
-                                                Key=path, ContentType=ContentType)
+    print(3)
+    response = s3.Bucket(bucketname).put_object(
+        ACL=acl, Body=_base64.b64encode(text.encode()), Key=path, ContentType=ContentType)
 
 
 if __name__ == '__main__':
     text = 'あいう生憎'
     bucketname = 'superfast.umihi.co'
     path = 'encoding.txt'
-    upload_text(bucketname, path, text)
+    upload_text(bucketname, path, text, public_read=True)
     print(download_text(bucketname, path))
